8a3fc8ec59b6023a9b5d7d13c9a194bcc47eaf06
mensafri <safrijawad1245@gmail.com>
2023-06-04 15:41:23 +0700

    acara dan tim

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,84 @@ import {
   ClipboardCheck,
   Clock,
   Loader,
+  X,
   XCircle,
 } from "lucide-react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+interface ItemAcaraType {
+  tgl: number
+  bulan: string
+  nama: string
+  tempat: string
+  waktu: string
+}
+interface ItemTimType {
+  nama: string
+  jabatan: string
+}
+
+const ItemTim = ({ nama, jabatan }: ItemTimType) => {
+  return (
+    <div className="flex flex-row">
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <div className="ml-4 flex flex-col">
+        <p className="font-bold">{nama}</p>
+        <p className="font-light">{jabatan}</p>
+      </div>
+    </div>
+  )
+}
+
+const ItemAcara = ({ tgl, bulan, nama, tempat, waktu }: ItemAcaraType) => {
+  return (
+    <div className="flex flex-row">
+      <div className="mr-4 flex flex-col items-center justify-center rounded-xl bg-[#FF5252] px-4 py-2 text-lg font-bold ">
+        <p className="text-3xl">{tgl}</p>
+        <p>{bulan}</p>
+      </div>
+      <div className="mr-4 flex flex-col">
+        <h1 className="font-bold">{nama}</h1>
+        <p className="mt-4 text-sm">{tempat}</p>
+        <p className="text-sm">{waktu}</p>
+      </div>
+      <div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="outline" className="w-10 rounded-full p-0">
+                <X className="h-4 w-4" />
+                <span className="sr-only">hapus</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Batalkan Acara</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  )
+}
 
 export default function IndexPage() {
   return (
     <>
       {/* Tugas dan Poin */}
       <div className="mb-0 ml-10 mt-6  flex w-8/12 flex-col">
-        <h1 className="text-4xl font-bold">Tugas</h1>
+        <h1 className="text-3xl font-bold">Tugas</h1>
         <div className="flex flex-row justify-between">
           <p className="font-light">Menampilkan semua tugas dari organisasi</p>
           <Link href="tugas">Lihat Semua</Link>
@@ -92,7 +158,7 @@ export default function IndexPage() {
             <div className="flex flex-col rounded-xl bg-[#FFFFFF] p-6"></div>
           </div>
         </div>
-        <h1 className="mt-2 text-4xl font-bold">Poin</h1>
+        <h1 className="mt-2 text-3xl font-bold">Poin</h1>
         <div className="flex flex-row justify-between">
           <p className="font-light">
             Menampilkan poin pelanggaran selama menjabat
@@ -105,8 +171,8 @@ export default function IndexPage() {
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <div className="mr-52 flex flex-col">
-            <p className="text font-bold">Yefi Afiatul Ardhani</p>
-            <p className="text font-light">Ketua</p>
+            <p className="font-bold">Yefi Afiatul Ardhani</p>
+            <p className="font-light">Ketua</p>
           </div>
           <div className="ml-96 rounded-xl bg-[#ffffff] px-4 py-2">0 Poin</div>
         </div>
@@ -115,18 +181,40 @@ export default function IndexPage() {
       {/* Acara dan Tim */}
       <div className="ml-16 p-5">
         <div className="flex flex-row justify-between">
-          <p className="font-bold">Acara</p>
-          <Link href="poin">Lihat Semua</Link>
+          <p className="text-3xl font-bold">Acara</p>
+          <Link href="acara">Lihat Semua</Link>
         </div>
-        <div className="mt-4 flex flex-row justify-between rounded-xl bg-[#F5F5F5] p-4">
-
+        <div className="mt-4 grid grid-cols-1 justify-between gap-4 rounded-xl bg-[#F5F5F5] p-4">
+          <ItemAcara
+            tgl={5}
+            bulan="Juni"
+            nama="Kajian Sore"
+            tempat="Kantin Unimus"
+            waktu="16.00 am - Selesai"
+          />
+          <ItemAcara
+            tgl={10}
+            bulan="Juni"
+            nama="Rapat Bid. PSDM"
+            tempat="Burjo Kekynian"
+            waktu="15.00 am - Selesai"
+          />
+          <ItemAcara
+            tgl={27}
+            bulan="Juni"
+            nama="Futsal Bareng"
+            tempat="Sport Center"
+            waktu="18.00 am - Selesai"
+          />
         </div>
         <div className="flex flex-row justify-between">
-          <p className="font-bold">Tim</p>
-          <Link href="poin">Lihat Semua</Link>
+          <p className="text-3xl font-bold">Tim</p>
+          <Link href="tim">Lihat Semua</Link>
         </div>
-        <div className="mt-4 flex flex-row justify-between rounded-xl bg-[#F5F5F5] p-4">
-
+        <div className="grid grid-cols-1 place-items-center justify-center gap-2 rounded-xl bg-[#F5F5F5] p-5">
+          <ItemTim nama="Yefi Afiatul Ardhani" jabatan="Ketua" />
+          <ItemTim nama="Taufikur Rohman" jabatan="Wakil Ketua" />
+          <ItemTim nama="Moch Ichwan" jabatan="Sekretaris II" />
         </div>
       </div>
     </>
