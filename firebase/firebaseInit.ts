4d945494df794d80b5309860ebfd502b110d0bf0
mensafri <safrijawad1245@gmail.com>
2023-06-17 22:26:14 +0700

    turu sek

--- firebase/firebaseInit.ts
+++ firebase/firebaseInit.ts
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+
+// import { getAnalytics } from "firebase/analytics"
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,10 +16,10 @@ const firebaseConfig = {
   storageBucket: "organizingweb.appspot.com",
   messagingSenderId: "101485347059",
   appId: "1:101485347059:web:d828e8b7805bbf99887786",
-  measurementId: "G-3L15LS64YS"
-};
+  measurementId: "G-3L15LS64YS",
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
 export const db = getFirestore(app)
--- firebase/users.ts
+++ firebase/users.ts
@@ -1,4 +1,24 @@
-import { addDoc, collection, getDocs } from "firebase/firestore"
+import {
+  GoogleAuthProvider,
+  NextOrObserver,
+  User,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signInWithRedirect,
+  signOut,
+} from "firebase/auth"
+import {
+  QueryDocumentSnapshot,
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore"
 
 import { addDivisions } from "./divisions"
 import { db } from "./firebaseInit"
@@ -33,4 +53,56 @@ export const getUsers = async () => {
   } catch (error) {
     console.log("error getting organizations", error)
   }
+}
+
+export const auth = getAuth()
+
+export type additionalInformation = {
+  displayName?: string
+  namaOrg: string
+  namaDiv: string
+}
+
+export type UserData = {
+  createdAt: Date
+  displayName: string
+  email: string
+}
+
+export const createUserDocumentFromAuth = async (
+  userAuth: User,
+  additionalInformation: additionalInformation
+): Promise<void | QueryDocumentSnapshot<UserData>> => {
+  if (!userAuth) return
+  const userDocRef = doc(db, "users", userAuth.uid)
+
+  const userSnapShot = await getDoc(userDocRef)
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      })
+    } catch (error) {
+      console.log("error di: ", error)
+    }
+  }
+
+  // return userDocRef;
+  return userSnapShot as QueryDocumentSnapshot<UserData>
+}
+
+export const createAuthUserWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  if (!email || !password) return
+
+  return await createUserWithEmailAndPassword(auth, email, password)
 }
